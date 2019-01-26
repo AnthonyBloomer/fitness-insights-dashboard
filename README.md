84deb92a060f452d8dcd5f49ae5175dc975a1a68
Anthony Bloomer <ant0@protonmail.ch>
2019-01-26 12:38:13 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fitness Insights Dashboard
+
+I have started to track my daily intake and exercise using MyFitnessPal and other mobile apps.  
+This project pulls data from those sources and sends to New Relic Insights. 
+This allows me to create visualisations and query my fitness and intake data using NRQL.
---
+++ fitnesspal.py
@@ -0,0 +1,88 @@
+import newrelic.agent
+
+newrelic.agent.initialize('newrelic.ini')
+newrelic.agent.register_application(timeout=10.0)
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+import os
+import json
+
+import datetime
+
+import myfitnesspal
+
+import schedule
+import time
+
+LOGIN_URL = "https://www.mapmywalk.com/auth/login/"
+WORKOUTS = "http://www.mapmywalk.com/workouts/dashboard.json?month=%s&year=%s"
+
+USERNAME = os.getenv('FITNESS_PAL_USERNAME')
+PASSWORD = os.getenv('FITNESS_PAL_PASSWORD')
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+
+@newrelic.agent.function_trace()
+def login():
+    driver.get(LOGIN_URL)
+    driver.find_element_by_id('email').send_keys(USERNAME)
+    driver.find_element_by_id('password').send_keys(PASSWORD)
+    driver.find_element_by_id('password').send_keys(Keys.ENTER)
+    return driver
+
+
+@newrelic.agent.function_trace()
+def get_workout_data():
+    d = login()
+    d.get(WORKOUTS)
+    pre = d.find_element_by_tag_name("pre").text
+    data = json.loads(pre)
+    wd = data['workout_data']['workouts']
+    today = datetime.date.today()
+    if today not in wd:
+        return None
+    else:
+        return wd[today]
+
+
+@newrelic.agent.function_trace()
+def send_workout_data():
+    application = newrelic.agent.application()
+    workout_data = get_workout_data()
+    if workout_data is not None:
+        for w in workout_data:
+            newrelic.agent.record_custom_event('WorkoutData', w, application)
+
+
+@newrelic.agent.function_trace()
+def send_intake_data():
+    client = myfitnesspal.Client(USERNAME, PASSWORD)
+    day = client.get_date(datetime.datetime.now())
+    application = newrelic.agent.application()
+    totals = day.totals
+    totals.update({
+        'water': day.water
+    })
+    newrelic.agent.record_custom_event('MealData', totals, application)
+
+
+@newrelic.agent.background_task()
+def job():
+    send_intake_data()
+    send_workout_data()
+
+
+if __name__ == '__main__':
+
+    schedule.every().day.at("23:30").do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
