713b4b4c098b2e2b0a20fb0757d872c33a7254e4
Anthony Bloomer <ant0@protonmail.ch>
2019-01-26 14:43:41 +0000

    Test

--- fitnesspal.py
+++ fitnesspal.py
@@ -27,6 +27,9 @@ chrome_options = Options()
 chrome_options.binary_location = GOOGLE_CHROME_BIN
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
 
